1c4d678a87f09e769f39efbaaa50738ce64bd54a
devaselviana <selvianadeva8@gmail.com>
2023-09-20 12:32:32 +0700

    Percobaan1

---
+++ Praktikum03/ContohVariabel08.java
@@ -0,0 +1,17 @@
+public class ContohVariabel08 {
+    public static void main(String[] args) {
+        String salahSatuHobiSayaAdalah = "Membaca";
+        boolean isPandai = true;
+        char jeniskelamin = 'K';
+        byte _umurSayaSekarang = 18;
+        double $ipk = 3.50, tinggi = 1.61;
+
+        System.out.println(salahSatuHobiSayaAdalah);
+        System.out.println("Apakah pandai?" + isPandai);
+        System.out.println("Jenis kelamin: " + jeniskelamin);
+        System.out.println("IPK: " + $ipk);
+        System.out.println(String.format("Saya berumur %s dengan tinggi badan %s", _umurSayaSekarang, tinggi));
+
+
+    }
+}
