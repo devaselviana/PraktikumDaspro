4d6f2cfc14ad9a3216ec3bfda3543ee91989bbf6
devaselviana <selvianadeva8@gmail.com>
2023-10-04 15:37:07 +0700

    PemilihanBilangan

--- Praktikum daspro jobsheet 5/PemilihanBilangan08.java
+++ Praktikum daspro jobsheet 5/PemilihanBilangan08.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class PemilihanBilangan08 {
 public static void main(String[] args) {
-   
     Scanner sc = new Scanner(System.in);
    
     System.out.print("Masukkan sebuah angka: ");
     int angka = sc.nextInt();
 
-    if (angka % 2 == 0)
+    String hasil = (angka % 2 == 0) ? "Angka " + angka + " termasuk bilangan genap" : "Angka " + angka + " termasuk bilangan ganjil";
+    System.out.println(hasil);
 
     {
-        System.out.println("Angka " + angka + " termasuk bilangan genap");
+    
     }
-    else
+    
     {
-        System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+    
     }
 
 }
--- Praktikum daspro jobsheet 5/PemilihanHariDenganIf08.java
+++ Praktikum daspro jobsheet 5/PemilihanHariDenganIf08.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+
 public class PemilihanHariDenganIf08 {
-    
+    public static void main(String[] args) {
+      
+        Scanner sc = new Scanner(System.in);
+    }
 }
