1b2363a120d5d496ae5100e4c1b8ad740f33e515
devaselviana <selvianadeva8@gmail.com>
2023-10-04 16:14:32 +0700

    PemilihanSwitch

--- Praktikum daspro jobsheet 5/PemilihanOperator08.java
+++ Praktikum daspro jobsheet 5/PemilihanOperator08.java
@@ -4,9 +4,17 @@ public class PemilihanOperator08 {
    public static void main(String[] args) {
     
     Scanner sc = new Scanner(System.in);
-    int angka1, angka2, hasil = 0;
+    double angka1, angka2, hasil = 0;
+    char operator;
 
-    if angka1
+    System.out.print("Masukkan angka1: ");
+    angka1 = sc.nextDouble();
+    System.out.print("Masukkan angka2: ");
+    angka2 = sc.nextDouble();
+    System.out.print("Masukkan operator (+ - * :)");
+    operator = sc.next().charAt(0);
+
+    if (operator)
 
    } 
 }
--- Praktikum daspro jobsheet 5/PemilihanSwitch08.java
+++ Praktikum daspro jobsheet 5/PemilihanSwitch08.java
@@ -29,10 +29,12 @@ public static void main(String[] args) {
             hasil = angka1/angka2;
             break;
         default: 
-    System.out.println("Maaf, operator yang dipilih tidak sesuai");
+    System.out.println("Maaf, operator yang dipilih tidak sesuai"); return;
+
     }
 
     System.out.println(angka1 + " " + operator + " " + angka2 + "=" + hasil);
+    sc.close();
 
 }    
 }
