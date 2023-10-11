ea018f4d568fe422847cb6adf9e664feab6679e9
unknown <selvianadeva8@gmail.com>
2023-10-11 14:14:24 +0700

    Segitiga08

--- Praktikum daspro jobsheet 6/Segitigaa08.java
+++ Praktikum daspro jobsheet 6/Segitigaa08.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
-public class Segitiga08 {
+public class Segitigaa08 {
     public static void main(String[] args) {
     Scanner input08 = new Scanner (System.in);
-        float sudut1,sudut2,sudut3;
-        int totalsudut;
+        int sudut1,sudut2,sudut3;
+        double totalsudut;
 
     System.out.println("Masukkan sudut1: ");
-    sudut1 = input08.nextFloat();
+    sudut1 = input08.nextInt();
     System.out.println("Masukkan sudut2: ");
-    sudut2 = input08.nextFloat();
+    sudut2 = input08.nextInt();
     System.out.println("Masukkan sudut3: ");
-    sudut3 = input08.nextFloat();
+    sudut3 = input08.nextInt();
 
     totalsudut = sudut1 + sudut2 + sudut3;
 
     if (totalsudut == 180) {
-        if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90));
+        if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
             System.out.println("Segitiga tersebut adalah segitiga siku-siku");
         else
             System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
-    }   else
-        System.out.println("Bukan Segitiga");   
+    } else
+        System.out.println("Bukan segitiga");
     }
 
 }
