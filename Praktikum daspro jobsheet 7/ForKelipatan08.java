13802d30092303386fe4155dd28d2fc13e2a60d8
devaselviana <selvianadeva8@gmail.com>
2023-10-18 14:30:16 +0700

    ForKelipatan08

--- Praktikum daspro jobsheet 7/ForKelipatan08.java
+++ Praktikum daspro jobsheet 7/ForKelipatan08.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ForKelipatan08 {
     public static void main(String[] args) {
         Scanner input08 = new Scanner (System.in); 
-        int bilangan,total = 0,counter = 0;
+        int bilangan,total=0,mean = 0,counter = 0;
 
 
         System.out.print("Masukkan bilangan (1-9): ");
@@ -11,11 +11,12 @@ public class ForKelipatan08 {
 
         for (int i = 1; i <= 50; ++i) {
             if (i % bilangan == 0) {
-                total += 1;
+                total += i;
                 ++counter;
+                mean = total/counter;
                
         System.out.printf("Pada rentang 1-50 terdapat %d bilangan kelipatan %d dengan total %d", counter, bilangan, total);
-        
+        System.out.printf("Rata-rata dari seluruh bilangan kelipatanadalah"+"" + mean);
             }
         }
     }
--- Praktikum daspro jobsheet 7/WhileKelipatan08.java
+++ Praktikum daspro jobsheet 7/WhileKelipatan08.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class WhileKelipatan08 {
     public static void main(String[] args) {
-        
+        Scanner input08 = new Scanner (System.in); 
+        int bilangan,total = 0,counter = 0;
+ 
     }
 }
