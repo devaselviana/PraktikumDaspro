c682b6e02422f2d0d9dcacbe525e99f537a836a4
devaselviana <selvianadeva8@gmail.com>
2023-11-01 15:49:36 +0700

    LinearSearchModif2

--- Praktikum daspro jobsheet 9/LinearSearch08.java
+++ Praktikum daspro jobsheet 9/LinearSearch08.java
@@ -1,10 +1,21 @@
 import java.util.Scanner;
 public class LinearSearch08 {
   public static void main(String[] args) {
-    
+    Scanner input08 = new Scanner (System.in);
+
     int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
     int key = 20;
     int hasil =0;
+
+    System.out.print("Masukkan jumlah elemen array : ");
+    int jumlahElemen = input08.nextInt();
+
+    for (int i = 0; i < arrayInt.length; i++){
+        System.out.print("Masukkan elemen  array ke-" +i+":");
+        arrayInt[i] = input08.nextInt();
+    }
+    System.out.print ("Masukkan key yang ingin di cari : ");
+    key = input08.nextInt();
 
     for (int i = 0; i < arrayInt.length; i++) {
         if (arrayInt[i] == key) {
@@ -12,7 +23,10 @@ public class LinearSearch08 {
             break;
         }
     }
-
-    System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
-  }  
+    if (hasil == 0){
+        System.out.println("Key tidak ditemukan");
+    } else{
+        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+    }  
 }   
+}
