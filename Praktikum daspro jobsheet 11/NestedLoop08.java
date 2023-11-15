b7d5bf82624d179f7674f91e28c1bccb4e926b22
devaselviana <selvianadeva8@gmail.com>
2023-11-15 17:42:23 +0700

    Praktikum Daspro Jobsheet 11

--- Praktikum daspro jobsheet 11/NestedLoop08.java
+++ Praktikum daspro jobsheet 11/NestedLoop08.java
@@ -3,23 +3,34 @@ public class NestedLoop08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double[][] suhu = new double[2][4];
+        double[] rataRata = new double[5];
 
         for (int i = 0; i < suhu.length; i++) {
             System.out.println("KOTA KE-" + (i+1));
+            double temp = 0;
 
             for (int j = 0; j < suhu[i].length; j++) {
-                System.out.print("  Suhu ke-" + (j + 1) + ": ");
+                System.out.print("Suhu ke-" + (j + 1) + ": ");
                 suhu[i][j] = sc.nextInt();
+                temp += suhu [i][j];
             }
-            System.out.println();
+            //System.out.println();
         }
         for (int i = 0; i < suhu.length; i++) {
             System.out.print("KOTA KE-" + (i+1) + ": " );
-
-            for (int j = 0; j < suhu[i].length; j++) {
-                System.out.print(suhu[i][j] + " ");
-            }
-            System.out.println();   
+            
+            //double total = 0;
+            //for (double j : suhu [i]){
+            //    System.out.print(j + " ");
+            //    total += j;
+            //}
+            //double rataRata = total / suhu[i].length;
+            //System.out.println("\nRata-Rata suhu : " + rataRata);
+            //System.out.println();
+            //for (int j = 0; j < suhu[i].length; j++) {
+                //System.out.print(suhu[i][j] + " ");
+            //}
+            //System.out.println();   
            
            }
              
