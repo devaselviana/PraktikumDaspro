02b943b1b7b5877541382998aabafbb89251017b
devaselviana <selvianadeva8@gmail.com>
2023-12-05 17:38:10 +0700

    Praktikum13

--- Praktikum13/CobaArgs08.java
+++ Praktikum13/CobaArgs08.java
@@ -4,8 +4,9 @@ public class CobaArgs08 {
         System.out.println("Hai " + args[0]);
         System.out.println("Selamat datang di " + args[1]);
 
-        //for (int i = 0; i < args.length; i++){
-        //    System.out.println(args[i]);
+        for (int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
         }
     }
+}
 
